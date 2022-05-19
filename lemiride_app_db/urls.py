be4e0cfd7b169709d64b40e90f4b6deab3a62b45
Anando Ghose <anandoghose@gmail.com>
2022-05-19 18:39:52 +0530

    working localities all option

--- lemiride_app_db/urls.py
+++ lemiride_app_db/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('customer-information/<str:number>', CustomerInformationViews.as_view()),
     path('localities/', LocalitiesViews.as_view()),
     path('product-details/', ProductDetailsViews.as_view()),
-    path('product-details/<str:location>-<int:day>-<int:month>-<int:year>-<int:hour>-<int:minute>', ProductDetailsViews.as_view()),
+    path('product-details/<str:location>-<str:city>-<int:day>-<int:month>-<int:year>-<int:hour>-<int:minute>', ProductDetailsViews.as_view()),
     path('user/', UserViews.as_view()),
     path('transaction-create/', TransactionCreate.as_view()),
 ]
